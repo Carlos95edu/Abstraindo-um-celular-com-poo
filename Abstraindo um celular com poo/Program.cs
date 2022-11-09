625791598870715af35a6af908d693c14c938efa
Carlos Eduardo <111974319+Carlos95edu@users.noreply.github.com>
2022-11-09 10:35:09 -0300

    Add files via upload

---
+++ Abstraindo um celular com poo/Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+Smartphone nokia = new Nokia("9 1234-5678", "3220","1548+8" , 16);
+Smartphone iphone = new Iphone("9 7890-1234","Iphone 11","asfg78",64);
+
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Snake");
+nokia.ExibirNumero();
+nokia.Numero= "9 7894-5612";
+nokia.ExibirNumero();
+
+iphone.InstalarAplicativo("Instagram");
+iphone.ReceberLigacao();
